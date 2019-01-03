d2b084e0cd8f87d0b4503b7d77bb59d23a00f896
植瑞 <zhirui@zhiruideMacBook-Pro.local>
2019-01-03 21:21:09 +0800

    添加BinaryString类

--- src/core/Tools.js
+++ src/core/Tools.js
@@ -2,10 +2,11 @@ import Type from "./Type/index.js";
 import of from "./lib/of";
 import Utils from "./Utils/index.js";
 import Exp from "./Exp/index.js";
+import BinaryString from './BinaryString/index.js';
 
 function Tools() {}
 
-Tools.prototype = Object.assign({}, Type, Exp, Utils, { of: of });
+Tools.prototype = Object.assign({}, Type, Exp, Utils, BinaryString, { of: of });
 Tools.prototype.constructor = Tools;
 
 export default Tools;
