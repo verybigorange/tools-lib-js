cf7262cd750427dacf4d8e5ddb8f1eb8bb586fb0
植瑞 <www.zhirui@foxmail.com>
2019-01-23 15:03:26 +0800

    添加对象深度继承方

---
+++ src/core/Object/index.js
@@ -0,0 +1,19 @@
+/**
+ * 通过继承的方式深拷贝对象。
+ * 源对象中的属性更改会导致生成对象的属性值更改，
+ * 但是生成对象的属性更改不会影响源对象。
+ * 即：父可以影响子，但子不可以影响父。 
+ */
+Object.clone = function(obj) {
+    const cloneObj = Object.create(obj);
+    for (let key of Object.keys(obj)) {
+        if (typeof obj[key] === 'object' || typeof obj[key] === 'function') {
+            cloneObj[key] = Object.clone(obj[key]);
+        }
+    }
+    return cloneObj;
+}
+
+export default {
+    clone: Object.clone,
+}
